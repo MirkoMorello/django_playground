0b3c7a7d11c835b2a0d11c60ca0b953dab21305e
Mirko Morello <mirkomorello97@gmail.com>
2022-05-04 22:22:47 +0200

    changed orderitem product related name

--- testProgetto/store/models.py
+++ testProgetto/store/models.py
@@ -100,7 +100,7 @@ class Address(models.Model): #relazione uno a molti
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems') # nome cambiato
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
